75fc0259eeebf02b0d2287779b8a672d6ceee445
karl <142769090+karlorz@users.noreply.github.com>
2025-09-27 19:28:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // Main entry point for Daily Login Assistant Bot
 class DailyLoginAssistant {
-    async start() {
-        console.log("Daily Login Assistant Bot starting...");
-    }
+  async start() {
+    console.log("Daily Login Assistant Bot starting...");
+  }
 }
 // Export instance
 exports.default = new DailyLoginAssistant();
